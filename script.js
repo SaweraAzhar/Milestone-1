46e39240a9154b17a82ecd16b2bde2039969eefb
SaweraAzhar <164591821+SaweraAzhar@users.noreply.github.com>
2024-09-08 06:30:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var _a;
+(_a = document.getElementById('toggle-skills')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
+    var skillsSection = document.getElementById('skills');
+    if ((skillsSection === null || skillsSection === void 0 ? void 0 : skillsSection.style.display) === 'none') {
+        skillsSection.style.display = 'block';
+    }
+    else {
+        skillsSection.style.display = 'none';
+    }
+});
